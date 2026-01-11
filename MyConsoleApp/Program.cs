f514ec83d2435fc7ed6ec3f2379b0fc01e1f331a
gotayjosue <gotayjosue@gmail.com>
2026-01-10 19:59:39 -0600

    My first .NET application

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿//Using System Globalization to format date and time
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+DateTime now = DateTime.Now;
+string formattedDate = now.ToString("hh:mm tt", new CultureInfo("en-US"));
+Console.WriteLine($"The current time is {formattedDate}");
+var daysUntilChristmas = (new DateTime(DateTime.Now.Year, 12, 25) - DateTime.Now).Days;
+Console.WriteLine($"There are {daysUntilChristmas} days until the next Christmas");
